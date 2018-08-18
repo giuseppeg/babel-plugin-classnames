5c8637bd7a1dbda09d550900479015ab97d1ad35
Brad Cornes <bradlc41@gmail.com>
2018-08-18 03:26:51 +0100

    add `importName` option to support non-default exports

--- index.js
+++ index.js
@@ -10,7 +10,14 @@ function babelPluginClassNames({ types: t }) {
         exit(path, state) {
           if (state.hasClassNames) {
             const importDeclaration = t.importDeclaration(
-              [t.importDefaultSpecifier(state.classNamesIdentifier)],
+              [
+                !state.opts.importName || state.opts.importName === 'default'
+                  ? t.importDefaultSpecifier(state.classNamesIdentifier)
+                  : t.importSpecifier(
+                    state.classNamesIdentifier,
+                    t.identifier(state.opts.importName)
+                  )
+              ],
               t.stringLiteral(state.opts.packageName || 'classnames')
             )
 
